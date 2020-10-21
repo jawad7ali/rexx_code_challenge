207d4217aa2755cd598a57d2e14eec0168be85b5
jawad ali <jawad7ali@gmail.com>
2020-10-21 19:18:54 +0500

    render data from json file

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sales</title>
+</head>
+<body>
+
+<form id="form">
+					<div style="text-align:right; padding-bottom:1em;">
+						<button type="submit">Submit form</button>
+					</div>
+<div id="example_wrapper" class="dataTables_wrapper" role="grid"><div id="example_length" class="dataTables_length"><label>Show <select size="1" name="example_length" aria-controls="example"><option value="10" selected="selected">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div><div class="dataTables_filter" id="example_filter"><label>Search: <input type="text" aria-controls="example"></label></div><table cellpadding="0" cellspacing="0" border="0" class="display dataTable" id="example" aria-describedby="example_info">
+	<thead>
+		<tr role="row"><th class="sorting_asc" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 116px;">Rendering engine</th><th class="sorting" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 157px;">Browser</th><th class="sorting" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 150px;">Platform(s)</th><th class="sorting" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 98px;">Engine version</th><th class="sorting" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 68px;">CSS grade</th><th class="sorting" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="Check: activate to sort column ascending" style="width: 43px;">Check</th></tr>
+	</thead>
+	
+	<tfoot>
+		<tr><th rowspan="1" colspan="1">Rendering engine</th><th rowspan="1" colspan="1">Browser</th><th rowspan="1" colspan="1">Platform(s)</th><th rowspan="1" colspan="1">Engine version</th><th rowspan="1" colspan="1">CSS grade</th><th rowspan="1" colspan="1">Check</th></tr>
+	</tfoot>
+<tbody role="alert" aria-live="polite" aria-relevant="all"><tr class="gradeA odd">
+			<td class="  sorting_1">Gecko</td>
+			<td class=" ">Firefox 1.0</td>
+			<td class=" ">Win 98+ / OSX.2+</td>
+			<td class="center ">1.7</td>
+			<td class="center ">A</td>
+			<td class="center "><input type="checkbox" name="check7" value="7"></td>
+		</tr><tr class="gradeA even">
+			<td class="  sorting_1">Gecko</td>
+			<td class=" ">Firefox 1.5</td>
+			<td class=" ">Win 98+ / OSX.2+</td>
+			<td class="center ">1.8</td>
+			<td class="center ">A</td>
+			<td class="center "><input type="checkbox" name="check8" value="8"></td>
+		</tr><tr class="gradeA odd">
+			<td class="  sorting_1">Gecko</td>
+			<td class=" ">Firefox 2.0</td>
+			<td class=" ">Win 98+ / OSX.2+</td>
+			<td class="center ">1.8</td>
+			<td class="center ">A</td>
+			<td class="center "><input type="checkbox" name="check9" value="9"></td>
+		</tr><tr class="gradeA even">
+			<td class="  sorting_1">Gecko</td>
+			<td class=" ">Firefox 3.0</td>
+			<td class=" ">Win 2k+ / OSX.3+</td>
+			<td class="center ">1.9</td>
+			<td class="center ">A</td>
+			<td class="center "><input type="checkbox" name="check10" value="10"></td>
+		</tr><tr class="gradeA odd">
+			<td class="  sorting_1">Gecko</td>
+			<td class=" ">Camino 1.0</td>
+			<td class=" ">OSX.2+</td>
+			<td class="center ">1.8</td>
+			<td class="center ">A</td>
+			<td class="center "><input type="checkbox" name="check11" value="11"></td>
+		</tr><tr class="gradeA even">
+			<td class="  sorting_1">Gecko</td>
+			<td class=" ">Camino 1.5</td>
+			<td class=" ">OSX.3+</td>
+			<td class="center ">1.8</td>
+			<td class="center ">A</td>
+			<td class="center "><input type="checkbox" name="check12" value="12"></td>
+		</tr><tr class="gradeA odd">
+			<td class="  sorting_1">Gecko</td>
+			<td class=" ">Netscape 7.2</td>
+			<td class=" ">Win 95+ / Mac OS 8.6-9.2</td>
+			<td class="center ">1.7</td>
+			<td class="center ">A</td>
+			<td class="center "><input type="checkbox" name="check13" value="13"></td>
+		</tr><tr class="gradeA even">
+			<td class="  sorting_1">Gecko</td>
+			<td class=" ">Netscape Browser 8</td>
+			<td class=" ">Win 98SE+</td>
+			<td class="center ">1.7</td>
+			<td class="center ">A</td>
+			<td class="center "><input type="checkbox" name="check14" value="14"></td>
+		</tr><tr class="gradeA odd">
+			<td class="  sorting_1">Gecko</td>
+			<td class=" ">Netscape Navigator 9</td>
+			<td class=" ">Win 98+ / OSX.2+</td>
+			<td class="center ">1.8</td>
+			<td class="center ">A</td>
+			<td class="center "><input type="checkbox" name="check15" value="15"></td>
+		</tr><tr class="gradeA even">
+			<td class="  sorting_1">Gecko</td>
+			<td class=" ">Mozilla 1.0</td>
+			<td class=" ">Win 95+ / OSX.1+</td>
+			<td class="center ">1</td>
+			<td class="center ">A</td>
+			<td class="center "><input type="checkbox" name="check16" value="16"></td>
+		</tr></tbody></table><div class="dataTables_info" id="example_info">Showing 1 to 10 of 57 entries</div><div class="dataTables_paginate paging_two_button" id="example_paginate"><a class="paginate_disabled_previous" tabindex="0" role="button" id="example_previous" aria-controls="example">Previous</a><a class="paginate_enabled_next" tabindex="0" role="button" id="example_next" aria-controls="example">Next</a></div></div>
+				</form>
+
+</body>
+</html>
