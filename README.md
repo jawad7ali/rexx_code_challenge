46fe9c14635d6bda114dc583dff18e375f73dab8
jawad ali <jawad7ali@gmail.com>
2020-11-02 17:41:00 +0500

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# rexx_code_challenge
+Instructions - 
+1. Update config.php file.
+2. Create database
+3. For import data from json file to database click on "Import Json to DB" link in index.html
+
---
+++ ajaxfile.php
@@ -0,0 +1,54 @@
+<?php
+include 'config.php';
+
+## Read value
+$draw = $_POST['draw'];
+$row = $_POST['start'];
+$rowperpage = $_POST['length']; // Rows display per page
+$columnIndex = $_POST['order'][0]['column']; // Column index
+$columnName = $_POST['columns'][$columnIndex]['data']; // Column name
+$columnSortOrder = $_POST['order'][0]['dir']; // asc or desc
+$searchValue = $_POST['search']['value']; // Search value
+
+## Search 
+$searchQuery = " ";
+if($searchValue != ''){
+	$searchQuery .= " and (customer_name like '%".$searchValue."%' or 
+        product_name like '%".$searchValue."%' or 
+        product_price like'%".$searchValue."%' ) ";
+}
+
+## Total number of records without filtering
+$sel = mysqli_query($con,"select count(*) as allcount from sales");
+$records = mysqli_fetch_assoc($sel);
+$totalRecords = $records['allcount'];
+
+## Total number of records with filtering
+$sel = mysqli_query($con,"select count(*) as allcount from sales WHERE 1 ".$searchQuery);
+$records = mysqli_fetch_assoc($sel);
+$totalRecordwithFilter = $records['allcount'];
+
+## Fetch records
+$empQuery = "select * from sales WHERE 1 ".$searchQuery." order by ".$columnName." ".$columnSortOrder." limit ".$row.",".$rowperpage;
+$empRecords = mysqli_query($con, $empQuery);
+$data = array();
+
+while ($row = mysqli_fetch_assoc($empRecords)) {
+    $data[] = array(
+    		"sale_id"=>$row['sale_id'],
+    		"customer_name"=>$row['customer_name'],
+    		"customer_mail"=>$row['customer_mail'],
+    		"product_name"=>$row['product_name'],
+    		"product_price"=>$row['product_price']
+    	);
+}
+
+## Response
+$response = array(
+    "draw" => intval($draw),
+    "iTotalRecords" => $totalRecords,
+    "iTotalDisplayRecords" => $totalRecordwithFilter,
+    "aaData" => $data
+);
+
+echo json_encode($response);
